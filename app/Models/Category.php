c28749efd8fc918377d5bfcefa0d2abb1f680424
Mehrshad Sahebsara <msahebsara@live.ca>
2021-01-23 13:12:15 -0700

    Add role, listing, category models and user role trait

---
+++ app/Models/Category.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Listing;
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['parent_id', 'name'];
+
+    public function children()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
+    public function listings()
+    {
+        return $this->hasMany(Listing::class);
+    }
+
+}
